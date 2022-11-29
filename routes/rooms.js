2552501ccaab6dba0e6e669d42fa294a1f8862f7
Asutosh Acharya <asutosh246@gmail.com>
2022-11-29 23:00:57 +0530

    backend api completed

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,7 +1,33 @@
 import express from "express";
+import {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getAllRooms,
+} from "./../controllers/room.js";
+import { verifyAdmin } from "./../utils/verifyToken.js";
 
 const router = express.Router();
 
+//create
 
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//update
+
+router.put("/:id", verifyAdmin, updateRoom);
+
+//delete
+
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//get
+
+router.get("/:id", getRoom);
+
+//get all
+
+router.get("/", getAllRooms);
 
 export default router;
